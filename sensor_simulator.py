898cea5661e5f726b2169762af47a84b1f772218
ginasus-glitch <gina.sus@sorsu.edu.ph>
2026-03-30 08:44:59 +0800

    Add SensorSimulator class with movement patterns

---
+++ sensor_simulator.py
@@ -0,0 +1,19 @@
+class SensorSimulator:
+    def __init__(self):
+        pass
+
+    def normal_movement(self):
+        # Implement normal movement pattern
+        pass
+
+    def minor_sway(self):
+        # Implement minor sway pattern
+        pass
+
+    def moderate_sway(self):
+        # Implement moderate sway pattern
+        pass
+
+    def severe_sway(self):
+        # Implement severe sway pattern
+        pass
